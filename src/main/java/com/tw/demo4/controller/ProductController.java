cd349c8f5b6b8a13fec96a3457acfae887b7a15d
eMe-404 <838702939>
2018-08-01 21:42:11 +0800

    feature : add get function

--- src/main/java/com/tw/demo4/controller/ProductController.java
+++ src/main/java/com/tw/demo4/controller/ProductController.java
@@ -5,10 +5,12 @@ import com.tw.demo4.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/products")
@@ -21,5 +23,14 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAll() {
         List<Product> products = productService.getAll();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> get(@PathVariable int id) {
+        Optional<Product> product = productService.get(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/tw/demo4/service/ProductService.java
+++ src/main/java/com/tw/demo4/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,5 +16,9 @@ public class ProductService {
 
     public List<Product> getAll() {
         return productRepository.findAll();
+    }
+
+    public Optional<Product> get(int id) {
+        return productRepository.findById(id);
     }
 }
